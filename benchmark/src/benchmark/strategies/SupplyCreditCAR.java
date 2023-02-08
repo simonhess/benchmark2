d8bd07d9b244ace2dbc960c44e58920999517402
Simon <Simon@DESKTOP-KEIOBOC.fritz.box>
2023-02-08 09:49:38 +0100

    Added credit supply strategy that considers car and expected profits

---
+++ benchmark/src/benchmark/strategies/SupplyCreditCAR.java
@@ -0,0 +1,125 @@
+/*
+ * JMAB - Java Macroeconomic Agent Based Modeling Toolkit
+ * Copyright (C) 2013 Alessandro Caiani and Antoine Godin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+package benchmark.strategies;
+
+import java.util.List;
+
+import benchmark.StaticValues;
+import benchmark.agents.Bank;
+import jmab.population.MacroPopulation;
+import jmab.stockmatrix.InterestBearingItem;
+import jmab.stockmatrix.Item;
+import jmab.stockmatrix.Loan;
+import jmab.strategies.SupplyCreditStrategy;
+import net.sourceforge.jabm.strategy.AbstractStrategy;
+
+/**
+ * @author Alessandro Caiani and Antoine Godin
+ *
+ */
+@SuppressWarnings("serial")
+public class SupplyCreditCAR extends AbstractStrategy implements
+		SupplyCreditStrategy {
+
+	/* (non-Javadoc)
+	 * @see jmab.strategies.SupplyCreditStrategy#computeCreditSupply()
+	 */
+	@Override
+	public double computeCreditSupply() {
+		
+		Bank b = (Bank) this.getAgent();
+		
+		
+		double expectedProfits = 0;
+
+		double depositsValue=0;
+		for(List<Item> list:b.getAssetStock()) {
+			for(Item i:list) {
+				if(i instanceof InterestBearingItem) {
+				InterestBearingItem iI = (InterestBearingItem) i;
+				expectedProfits+=iI.getValue()*iI.getInterestRate();
+				}
+			}
+			
+		}
+
+		for(List<Item> list:b.getLiabilityStock()) {
+			for(Item i:list) {
+				if(i instanceof InterestBearingItem) {
+				InterestBearingItem iI = (InterestBearingItem) i;
+				expectedProfits-=iI.getValue()*iI.getInterestRate();
+				}
+			}
+			
+		}
+		ProfitsWealthTaxStrategy strategy = (ProfitsWealthTaxStrategy) b.getStrategy(StaticValues.STRATEGY_TAXES);
+		double expectedAfterTaxProfits = expectedProfits*strategy.getProfitTaxRate();
+		double expAfterTaxProfits=expectedAfterTaxProfits;
+		
+		double expNW = b.getPassedValue(StaticValues.LAG_NETWEALTH, 1)+expAfterTaxProfits;
+		
+		// Calculate principal payment
+		
+		List<Item> loans=b.getItemsStockMatrix(true, StaticValues.SM_LOAN);
+		double principalSum=0;
+		double loanSum=0;
+		for(int i=0;i<loans.size();i++){
+			Loan loan=(Loan)loans.get(i);
+			if(loan.getAge()>0){
+				double iRate=loan.getInterestRate();
+				double amount=loan.getInitialAmount();
+				int length = loan.getLength();
+				double interests=iRate*loan.getValue();
+				
+				double principal=0.0;
+				switch(loan.getAmortization()){
+				case Loan.FIXED_AMOUNT:
+					double amortization = amount*(iRate*Math.pow(1+iRate, length))/(Math.pow(1+iRate, length)-1);
+					principal=amortization-interests;
+					break;
+				case Loan.FIXED_CAPITAL:
+					principal=amount/length;
+					break;
+				case Loan.ONLY_INTERESTS:
+					if(length==loan.getAge())
+						principal=amount;
+					break;
+				}
+				principalSum+=principal;
+				loanSum+=loan.getValue();
+			}
+		}	
+		
+		double maxLoans = expNW/b.getTargetedCapitalAdequacyRatio();
+		double maxLoansSupply=maxLoans-loanSum+principalSum;
+		
+		return maxLoansSupply;
+	}
+
+	/* (non-Javadoc)
+	 * @see jmab.strategies.SingleStrategy#getBytes()
+	 */
+	@Override
+	public byte[] getBytes() {
+		return new byte[1];//TODO
+	}
+
+	/* (non-Javadoc)
+	 * @see jmab.strategies.SingleStrategy#populateFromBytes(byte[], jmab.population.MacroPopulation)
+	 */
+	@Override
+	public void populateFromBytes(byte[] content, MacroPopulation pop) {}
+
+}
